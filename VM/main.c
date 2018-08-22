64aef84bd9d9e21e767a280913a79ecefac5f636
Vitalii BONDARENKO <vibondar@e1r3p1.unit.ua>
2018-08-22 15:34:04 +0300

    fail

---
+++ VM/main.c
@@ -0,0 +1,35 @@
+#include "vm.h"
+
+int manage_flags(char arg, char **argv, int i)
+{
+	return(i);
+}
+
+int manage_files(char arg, char **argv, int i)
+{
+	int fd;
+
+	fd = open(arg);
+	return(i);
+}
+
+int main(int argc, char **argv)
+{
+	t_cor cor;
+	char *arg;
+	int i;
+
+	i = 1;
+	while(i < argc)
+	{
+		arg = ft_strtrim(argv[i]);
+		if(arg[0] == '-')
+			i = manage_flags(arg, argv, i);
+		elseit
+			i = manage_files(arg, argv, i);
+		free(arg);
+		i++;
+	}
+	system("leaks -quiet corewar");
+	return(0);
+}
