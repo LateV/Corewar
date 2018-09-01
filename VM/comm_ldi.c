b16aef4625f05ee14aaea027a3bbd5c755d899be
Maryna POPOVYCH <mpopovyc@e1r3p3.unit.ua>
2018-09-01 18:10:58 +0300

    sub

--- VM/comm_ldi.c
+++ VM/comm_ldi.c
@@ -1,9 +1,67 @@
 #include "vm.h"
 
+static int arg_read(t_cor *cor, t_process *process, int *name)
+{
+	int s;
+
+	s = 2;
+	if(process->arg1 == 0)
+		process->codage = 0;
+	if (process->arg2 == 3 || process->arg2 == 0)
+		process->codage = 0;
+	if (process->arg3 != 1)
+		process->codage = 0;
+	if (process->arg1 == 3)
+		*name = 3;
+	s = arg_handler(cor, process, &process->arg1, s);
+	s = arg_handler(cor, process, &process->arg2, s);
+	s = arg_handler(cor, process, &process->arg3, s);
+	return(s);
+}		
+
 void comm_ldi(t_cor *cor, t_process *process)
 {
-	printf("pos = %d\n", process->pc);
-	printf("%02x\n", cor->arena[process->pc]);
-	printf("%s\n", "ldi");
-	set_proc_pos(process, 1);
+	int sk;
+	int name;
+	int sum;
+	int first;
+
+	name = 0;
+	if (process->delay < 0)
+		process->delay = 24;
+	else if (process->delay > 0)
+		process->delay--;
+	if (process->delay == 0)
+	{
+		process->label = 4;
+		codage_identify(process, get_char(cor, process->pc + 1));
+		process->codage = 1;
+		sk = arg_read(cor, process, &name);
+		if(process->codage == 1)
+		{
+			first = get_reg(process, process->arg1);
+			if (name == 3)
+			{
+				first = load_from_reg(cor, process, 4, get_reg(process, process->arg1) % IDX_MOD + process->pc);
+				ft_putstr("-> ldi ");
+				ft_putnbr(get_reg(process, process->arg2 - 1));
+				ft_putstr(" r");
+				ft_putnbr(process->arg2);
+				ft_putstr("\n");
+			}
+			sum = first + get_reg(process, process->arg2) % IDX_MOD + process->pc; 
+			process->regist[process->arg3 - 1] = load_from_reg(cor, process, 4, sum);
+			ft_putstr("->ldi: load from ");
+			ft_putnbr(first);
+			ft_putstr(" + ");
+			ft_putnbr(p);
+			ft_putstr(" = ");
+			ft_putnbr(process->arg2 + process->arg3);
+			ft_putstr("\n");
+		}
+		set_proc_pos(process, sk);
+		process->delay = -1;
+		process->codage = 1;
+		process->command = -1;
+	}
 }
