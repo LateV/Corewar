b0298d85db8653ab0880534cfcbe836811d0c383
Roman KHILENKO <rkhilenk@e1r3p3.unit.ua>
2018-09-21 16:52:34 +0300

    vizu norminetted

--- VM/src/v_main.c
+++ VM/src/v_main.c
@@ -12,36 +12,31 @@
 
 #include "vm.h"
 
-static void itit_color_pairs(void)
+static void	itit_color_pairs(void)
 {
-		// standart players color
-		init_pair(1, COLOR_GREEN, COLOR_BLACK);
-		init_pair(2, COLOR_BLUE, COLOR_BLACK);
-		init_pair(3, COLOR_RED, COLOR_BLACK);
-		init_pair(4, COLOR_CYAN, COLOR_BLACK);
-		// carret players color
-		init_pair(5, COLOR_BLACK, COLOR_GREEN);
-		init_pair(6, COLOR_BLACK, COLOR_BLUE);
-		init_pair(7, COLOR_BLACK, COLOR_RED);
-		init_pair(8, COLOR_BLACK, COLOR_CYAN);
-		// player copy color
-		init_pair(9, 41, COLOR_BLACK);
-		init_pair(10, 27, COLOR_BLACK);
-		init_pair(11, 160, COLOR_BLACK);
-		init_pair(12, 14, COLOR_BLACK);
-		// life scream
-		init_pair(13, COLOR_WHITE, COLOR_GREEN);
-		init_pair(14, COLOR_WHITE, COLOR_BLUE);
-		init_pair(15, COLOR_WHITE, COLOR_RED);
-		init_pair(16, COLOR_WHITE, COLOR_CYAN);
-		// no players colors
-		init_pair(22, 243, COLOR_BLACK);
-		init_pair(23, COLOR_BLACK, 243);
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
+	init_pair(2, COLOR_BLUE, COLOR_BLACK);
+	init_pair(3, COLOR_RED, COLOR_BLACK);
+	init_pair(4, COLOR_CYAN, COLOR_BLACK);
+	init_pair(5, COLOR_BLACK, COLOR_GREEN);
+	init_pair(6, COLOR_BLACK, COLOR_BLUE);
+	init_pair(7, COLOR_BLACK, COLOR_RED);
+	init_pair(8, COLOR_BLACK, COLOR_CYAN);
+	init_pair(9, 41, COLOR_BLACK);
+	init_pair(10, 27, COLOR_BLACK);
+	init_pair(11, 160, COLOR_BLACK);
+	init_pair(12, 14, COLOR_BLACK);
+	init_pair(13, COLOR_WHITE, COLOR_GREEN);
+	init_pair(14, COLOR_WHITE, COLOR_BLUE);
+	init_pair(15, COLOR_WHITE, COLOR_RED);
+	init_pair(16, COLOR_WHITE, COLOR_CYAN);
+	init_pair(22, 243, COLOR_BLACK);
+	init_pair(23, COLOR_BLACK, 243);
 }
 
-void initital_draw(t_cor *cor)
+void		initital_draw(t_cor *cor)
 {
-	if(cor->visu == 1)
+	if (cor->visu == 1)
 	{
 		box(cor->vizu->win1, 0, 0);
 		box(cor->vizu->win2, 0, 0);
@@ -49,76 +44,62 @@ void initital_draw(t_cor *cor)
 			mvwprintw(cor->vizu->win2, 2, 2, "%-20s", "** PAUSED **");
 		else
 			mvwprintw(cor->vizu->win2, 2, 2, "%-20s", "** PLAY **");
-		mvwprintw(cor->vizu->win2, 4, 2, "Cycles/second limit : %-9d", 1000000 / cor->vizu->speed);
+		mvwprintw(cor->vizu->win2, 4, 2, "Cycles/second limit : %-9d",
+		1000000 / cor->vizu->speed);
 		mvwprintw(cor->vizu->win2, 7, 2, "Cycle : %-20d", (cor->cycles) - 1);
-		mvwprintw(cor->vizu->win2, 9, 2, "Processes : %2d", (cor->proc_num) - 1);
-		
-		
+		mvwprintw(cor->vizu->win2, 9, 2, "Processes : %-20d",
+		(cor->proc_num) - 1);
 	}
 }
 
-void draw_palyer_info(t_cor *cor, t_process *proc, int k)
+void		draw_palyer_info(t_cor *cor, t_process *proc, int k)
 {
-	if(cor->visu == 1)
+	if (cor->visu == 1)
 	{
-		mvwprintw(cor->vizu->win2, 11 + (k * 4), 2, "Player %d : ", proc->player->num);
-		wattron(cor->vizu->win2, COLOR_PAIR(k+1));
-		mvwprintw(cor->vizu->win2, 11 + (k * 4), 14, "%.40s", proc->player->prog_name);
-		wattroff(cor->vizu->win2, COLOR_PAIR(k+1));
+		mvwprintw(cor->vizu->win2, 11 + (k * 4), 2, "Player %d : ",
+		proc->player->num);
+		wattron(cor->vizu->win2, COLOR_PAIR(k + 1));
+		mvwprintw(cor->vizu->win2, 11 + (k * 4), 14, "%.40s",
+		proc->player->prog_name);
+		wattroff(cor->vizu->win2, COLOR_PAIR(k + 1));
 		mvwprintw(cor->vizu->win2, 12 + (k * 4), 4, "Last live : %20d", 0);
-		mvwprintw(cor->vizu->win2, 13 + (k * 4), 4, "Lives in current period : %6d", 0);
+		mvwprintw(cor->vizu->win2, 13 + (k * 4), 4,
+		"Lives in current period : %6d", 0);
 		cor->vizu->end_of_prs++;
 	}
 }
 
-void draw_info(t_cor *cor)
+void		draw_info(t_cor *cor)
 {
-	if(cor->visu == 1)
-	{
-		int k;
+	int k;
 
+	if (cor->visu == 1)
+	{
 		k = cor->vizu->end_of_prs;
-		mvwprintw(cor->vizu->win2, 15 + ((k-1) * 4), 2, "Live breakdown for current period :");
-		mvwprintw(cor->vizu->win2, 16 + ((k-1) * 4), 2, "[--------------------------------------------------]");
-		mvwprintw(cor->vizu->win2, 18 + ((k-1) * 4), 2, "Live breakdown for last period :");
-		mvwprintw(cor->vizu->win2, 19 + ((k-1) * 4), 2, "[--------------------------------------------------]");
-		// mvwprintw(cor->vizu->win2, 21 + ((k-1) * 4), 2, "CYCLE_TO_DIE : %d", cor->curr_cycle_t_d);
-		mvwprintw(cor->vizu->win2, 21 + ((cor->vizu->end_of_prs-1) * 4), 2, "CYCLE_TO_DIE : %d", cor->curr_cycle_t_d);
-		mvwprintw(cor->vizu->win2, 23 + ((k-1) * 4), 2, "CYCLE_DELTA : %d", CYCLE_DELTA);
-		mvwprintw(cor->vizu->win2, 25 + ((k-1) * 4), 2, "NBR_LIVE : %d", NBR_LIVE);
-		mvwprintw(cor->vizu->win2, 27 + ((k-1) * 4), 2, "MAX_CHECKS : %d", MAX_CHECKS);
+		mvwprintw(cor->vizu->win2, 15 + ((k - 1) * 4), 2,
+		"Live breakdown for current period :");
+		mvwprintw(cor->vizu->win2, 16 + ((k - 1) * 4), 2,
+		"[--------------------------------------------------]");
+		mvwprintw(cor->vizu->win2, 18 + ((k - 1) * 4), 2,
+		"Live breakdown for last period :");
+		mvwprintw(cor->vizu->win2, 19 + ((k - 1) * 4), 2,
+		"[--------------------------------------------------]");
+		mvwprintw(cor->vizu->win2, 21 + ((cor->vizu->end_of_prs - 1) * 4), 2,
+		"CYCLE_TO_DIE : %d", cor->curr_cycle_t_d);
+		mvwprintw(cor->vizu->win2, 23 + ((k - 1) * 4), 2,
+		"CYCLE_DELTA : %d", CYCLE_DELTA);
+		mvwprintw(cor->vizu->win2, 25 + ((k - 1) * 4), 2,
+		"NBR_LIVE : %d", NBR_LIVE);
+		mvwprintw(cor->vizu->win2, 27 + ((k - 1) * 4), 2,
+		"MAX_CHECKS : %d", MAX_CHECKS);
 	}
 }
 
-void put_com(t_cor *cor, int pos, unsigned char comm, int color)
-{
-	if(cor->visu == 1)
-	{
-		color = (color * (-1))-1;
-		wattron(cor->vizu->win1, COLOR_PAIR(color+1));
-
-		mvwprintw(cor->vizu->win1, (pos / 64) + 1, (pos % 64) * 3 + 2, "%02x", comm);
-
-		wattroff(cor->vizu->win1, COLOR_PAIR(color+1));
-	}
-}
-
-void put_car(t_cor *cor, int pos, unsigned char comm, int color)
-{
-	if(cor->visu == 1)
-	{
-		color = (color * (-1))-1;
-		wattron(cor->vizu->win1, COLOR_PAIR(color+5));
-		mvwprintw(cor->vizu->win1, (pos / 64) + 1, (pos % 64) * 3 + 2, "%02x", comm);
-		wattroff(cor->vizu->win1, COLOR_PAIR(color+5));
-	}
-}
-
-void init_window(t_cor *cor)
+void		init_window(t_cor *cor)
 {
 	t_vizu	*vizu;
 
-	if(cor->visu == 1)
+	if (cor->visu == 1)
 	{
 		vizu = malloc(sizeof(t_vizu));
 		cor->vizu = vizu;
@@ -134,78 +115,7 @@ void init_window(t_cor *cor)
 		cor->vizu->win1 = newwin(66, 195, 0, 0);
 		cor->vizu->win2 = newwin(66, 56, 0, 196);
 		itit_color_pairs();
-
 		init_map(cor);
 		initital_draw(cor);
 	}
-
-	// mvwprintw(cor->vizu->win2, 9, 2, "qweqweqwe", 1);
-	// wrefresh(cor->vizu->win2);
-
-	// while (1)
-	// {
-	// 	getch();
-
-	// 	wrefresh(cor->vizu->win1);
-	// 	wrefresh(cor->vizu->win2);
-	// }
 }
-// int main(void)
-// {
-// 	initscr();
-// 	start_color();
-// 	noecho();
-// 	keypad(stdscr, true);
-// 	curs_set(false);
-// 	char arr[MEM_SIZE];
-
-// 	timeout(0);
-// 	arr[0] = 90;
-// 	arr[1] = 123;
-// 	arr[2] = 0;
-
-// 	init_pair(1, COLOR_RED, COLOR_BLACK);
-//     init_pair(2, COLOR_BLACK, COLOR_RED);
-	
-// 	// printf("%s\n", ft_itoa_base(int(arr[0]), 16));
-// 	// printw("Hello world!\n");  // Отображение приветствия в буфер
-//     write(1, "\e[8;60;200;t", 14);                
-
-// 	WINDOW * win1 = newwin(60, 150, 0, 0);
-// 	WINDOW * win2 = newwin(60, 50, 0, 150);
-	
-// 	box(win1, 0, 0);
-// 	box(win2, 0, 0);
-
-// 	wattron(win1, COLOR_PAIR(2));
-// 	mvwprintw(win1, 1, 2, "%02x", arr[0]);
-// 	wattroff(win1, COLOR_PAIR(2));
-
-
-// 	wattron(win1, COLOR_PAIR(1));
-// 	mvwprintw(win1, 1, 5, "%02x", arr[1]);
-// 	mvwprintw(win1, 1, 8, "%02x", arr[2]);
-// 	wattroff(win1, COLOR_PAIR(1));
-
-
-// 	wattron(win2, COLOR_PAIR(1));
-// 	mvwprintw(win2, 1, 2, "legend side");
-// 	wattroff(win2, COLOR_PAIR(1));
-
-
-// 	while (1)
-// 	{
-		// getch();
-
-// 		wrefresh(win1);
-// 		wrefresh(win2);
-// 	}
-	
-
-
-// 	endwin();
-// 	return 0;
-// }
-
-
-// gcc -lncurses v_main.c ../libft/libft.a
--- VM/src/v_map_handler.c
+++ VM/src/v_map_handler.c
@@ -12,330 +12,39 @@
 
 #include "vm.h"
 
-void 			refresher(t_cor *cor)
-{
-	int i;
-
-	i = -1;
-	while (++i < 4096)
-	{
-		if (cor->vizu->map[i].life_time > 0 && (cor->pause == 0 || cor->cycles < cor->start_from))
-			cor->vizu->map[i].life_time--;
-		if (cor->vizu->map[i].life_scream > 0 && (cor->pause == 0 || cor->cycles < cor->start_from))
-		{
-			cor->vizu->map[i].life_scream--;
-			// if (cor->vizu->map[i].life_scream <= 0)
-			// 	cor->vizu->map[i].player = cor->vizu->map[i].prev_player;
-		}
-	}
-}
-
-void refresh_map(t_cor *cor)
-{
-	int i;
-	
-
-	i = -1;
-	cor->stealth = 0;
-
-	while (++i < 4096)
-	{
-		if (cor->vizu->map[i].player == -1 && cor->vizu->map[i].type == 1)
-		{
-			wattron(cor->vizu->win1, COLOR_PAIR(23));
-			mvwprintw(cor->vizu->win1, (i / 64) + 1, (i % 64) * 3 + 2, "%02x", cor->vizu->map[i].comm);
-			wattroff(cor->vizu->win1, COLOR_PAIR(23));
-		}
-		else if (cor->vizu->map[i].player == -1)
-		{
-			wattron(cor->vizu->win1, COLOR_PAIR(22));
-			mvwprintw(cor->vizu->win1, (i / 64) + 1, (i % 64) * 3 + 2, "%02x", cor->vizu->map[i].comm);
-			wattroff(cor->vizu->win1, COLOR_PAIR(22));
-		}
-		else if (cor->vizu->map[i].life_scream > 0)
-		{
-			wattron(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].car_player + 12));
-			mvwprintw(cor->vizu->win1, (i / 64) + 1, (i % 64) * 3 + 2, "%02x", cor->vizu->map[i].comm);
-			wattroff(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].car_player + 12));
-		}
-		else if (cor->vizu->map[i].type == 1)
-		{
-			wattron(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].player + 4));
-			mvwprintw(cor->vizu->win1, (i / 64) + 1, (i % 64) * 3 + 2, "%02x", cor->vizu->map[i].comm);
-			wattroff(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].player + 4));
-		}
-		else if (cor->vizu->map[i].life_time > 0)
-		{
-			wattron(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].player + 8));
-			mvwprintw(cor->vizu->win1, (i / 64) + 1, (i % 64) * 3 + 2, "%02x", cor->vizu->map[i].comm);
-			wattroff(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].player + 8));
-		}
-		else
-		{
-			wattron(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].player));
-			mvwprintw(cor->vizu->win1, (i / 64) + 1, (i % 64) * 3 + 2, "%02x", cor->vizu->map[i].comm);
-			wattroff(cor->vizu->win1, COLOR_PAIR(cor->vizu->map[i].player));
-		}
-
-		if (cor->stealth == 1)
-			cor->vizu->map[i].comm = 255;
-	}
-	
-}
-
-void v_speed_test(t_cor *cor, char t)
-{
-
-	// speed 0 = 500 000 2
-	// speed 1 = 100 000 10
-	// speed 2 = 50 000  20
-	// speed 3 = 20 000  50
-	// speed 4 = 10 000  100
-	// speed 5 = 1 000   1000
-
-	if (t == '=' && cor->vizu->speed == 500000)
-		cor->vizu->speed = 100000;
-	else if (t == '=' && cor->vizu->speed == 100000)
-		cor->vizu->speed = 50000;
-	else if (t == '=' && cor->vizu->speed == 50000)
-		cor->vizu->speed = 20000;
-	else if (t == '=' && cor->vizu->speed == 20000)
-		cor->vizu->speed = 10000;
-	else if (t == '=' && cor->vizu->speed == 10000)
-		cor->vizu->speed = 1000;
-
-	if (t == '-' && cor->vizu->speed == 1000)
-		cor->vizu->speed = 10000;
-	else if (t == '-' && cor->vizu->speed == 10000)
-		cor->vizu->speed = 20000;
-	else if (t == '-' && cor->vizu->speed == 20000)
-		cor->vizu->speed = 50000;
-	else if (t == '-' && cor->vizu->speed == 50000)
-		cor->vizu->speed = 100000;
-	else if (t == '-' && cor->vizu->speed == 100000)
-		cor->vizu->speed = 500000;
-
-	if (cor->vizu->speed == 100000)
-		mvwprintw(cor->vizu->win2, 30 + ((cor->vizu->end_of_prs-1) * 4), 2, "speed : >      ", cor->vizu->speed);
-	else if (cor->vizu->speed == 50000)
-		mvwprintw(cor->vizu->win2, 30 + ((cor->vizu->end_of_prs-1) * 4), 2, "speed : >>     ", cor->vizu->speed);
-	else if (cor->vizu->speed == 20000)
-		mvwprintw(cor->vizu->win2, 30 + ((cor->vizu->end_of_prs-1) * 4), 2, "speed : >>>    ", cor->vizu->speed);
-	else if (cor->vizu->speed == 10000)
-		mvwprintw(cor->vizu->win2, 30 + ((cor->vizu->end_of_prs-1) * 4), 2, "speed : >>>>   ", cor->vizu->speed);
-	else if (cor->vizu->speed == 1000)
-		mvwprintw(cor->vizu->win2, 30 + ((cor->vizu->end_of_prs-1) * 4), 2, "speed : max    ", cor->vizu->speed);
-	else if (cor->vizu->speed == 500000)
-		mvwprintw(cor->vizu->win2, 30 + ((cor->vizu->end_of_prs-1) * 4), 2, "speed : min    ", cor->vizu->speed);
-}
-
-void refresh_info(t_cor *cor)
-{
-	mvwprintw(cor->vizu->win2, 21 + ((cor->vizu->end_of_prs-1) * 4), 2, "CYCLE_TO_DIE : %-6d", cor->curr_cycle_t_d);
-}
-
-void refresh_player(t_cor *cor)
+void	refresh_player(t_cor *cor)
 {
 	int i;
 
 	i = 0;
-	while(i < cor->p_num)
+	while (i < cor->p_num)
 	{
-		mvwprintw(cor->vizu->win2, 12 + (i * 4), 16, "%20d", cor->player[i].last_live);
-		mvwprintw(cor->vizu->win2, 13 + (i * 4), 30, "%6d", cor->player[i].live_summ);
+		mvwprintw(cor->vizu->win2, 12 + (i * 4),
+		16, "%20d", cor->player[i].last_live);
+		mvwprintw(cor->vizu->win2, 13 + (i * 4),
+		30, "%6d", cor->player[i].live_summ);
 		i++;
 	}
 }
 
-void off_rounder(t_cor *cor, int res_summ)
-{
-	int	max_score = 0;
-	int max_player = 0;
-	int i;
-
-	i = -1;
-	while (++i < cor->p_num)
-	{
-		if (cor->vizu->brakedown[i] > max_score)
-		{
-			max_score = cor->vizu->brakedown[i];
-			max_player = i;
-		}
-	}
-	cor->vizu->brakedown[max_player] += (50 - res_summ);
-}
-
-void break_printer(t_cor *cor, int array[4], int line)
-{
-	int carret = 0;
-	int i = -1;
-
-	mvwprintw(cor->vizu->win2, line + ((cor->p_num-1) * 4), 2, "[--------------------------------------------------]");
-	while (++i < cor->p_num)
-	{
-		if (array[i] > 0)
-		{
-			wattron(cor->vizu->win2, COLOR_PAIR(i + 1));
-			mvwprintw(cor->vizu->win2, line + ((cor->p_num-1) * 4), 3 + carret, "%.*s", array[i] , "--------------------------------------------------");
-			wattroff(cor->vizu->win2, COLOR_PAIR(i + 1));
-			carret += array[i];
-		}
-	}
-
-}
-
-void breakdown_cpy(t_cor *cor)
-{
-	int i = -1;
-
-	while (++i < cor->p_num)
-		cor->vizu->last_brakedown[i] = cor->vizu->brakedown[i];
-
-
-}
-
-void breakdown(t_cor *cor)
-{
-	int i = -1;
-	double summ = 0;
-	int res_summ = 0;
-	while (++i < cor->p_num)
-		summ += cor->player[i].live_summ;
-	i = -1;
-	if (cor->live_check == cor->curr_cycle_t_d)
-	{
-		breakdown_cpy(cor);
-		system("afplay ./sound.mp3&");
-	}
-	if (summ != 0)
-	{
-		while (++i < cor->p_num)
-		{
-			if (cor->player[i].live_summ != 0)
-			{
-				cor->vizu->brakedown[i] = (50 / summ) * cor->player[i].live_summ;
-				res_summ += cor->vizu->brakedown[i];
-			}
-			else
-				cor->vizu->brakedown[i] = 0;
-		}
-	}
-	if (res_summ != 0 && res_summ != 50)
-		off_rounder(cor, res_summ);
-	
-
-}
-
-void refresh_vizu(t_cor *cor)
+void	refresh_vizu(t_cor *cor)
 {
 	initital_draw(cor);
-
-	
-	refresh_info(cor);
+	mvwprintw(cor->vizu->win2, 21 + ((cor->vizu->end_of_prs - 1) * 4),
+	2, "CYCLE_TO_DIE : %-6d", cor->curr_cycle_t_d);
 	refresh_map(cor);
 	refresh_player(cor);
-
-	
 	break_printer(cor, cor->vizu->brakedown, 16);
 	break_printer(cor, cor->vizu->last_brakedown, 19);
-
 	wrefresh(cor->vizu->win1);
 	wrefresh(cor->vizu->win2);
-
 	if (cor->pause == 0)
 		usleep(cor->vizu->speed);
 	else
 		usleep(1000);
 }
 
-void win_art_sword(t_cor *cor, int y)
-{
-	int x = 10;
-
-	wattron(cor->vizu->win1, COLOR_PAIR((cor->winner->num) * (-1) + 8));
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "-----------------------------------------");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                   ^                   |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                  / \\                  |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                 / ^ \\                 |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                / / \\ \\                |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|               / / ^ \\ \\               |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|              / / / \\ \\ \\              |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|             | |  \\ /  | |             |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|             | |  |V|  | |             |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|             / /  |||  \\ \\             |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|            | |   |||   | |            |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|            | |   |||   | |            |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|            / |   |||   | \\            |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|    ___    |  /   |||   \\  |    ___    |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|    \\  \\__ | |    |||    | | __/  /    |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|\\    \\  _ `' |    |||    | `' _  /    /|");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "| \\    \\ \\`_  |    |||    |  _'/ /    / |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|  \\    \\ \\ `.'    |||    `.' / /    /  |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|   \\    \\ \\       |||       / /    /   |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|    \\    \\ \\      |||      / /    /    |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|     \\    \\ \\     |||     / /    /     |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|      \\    \\ \\    |||    / /    /      |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|       \\    \\ \\   |||   / /    /       |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|        \\    \\ \\  |||  / /    /        |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|         \\    \\ \\ ||| / /    /         |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|          \\    \\ \\|||/ /    /          |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|           \\  (========)   /           |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|            \\     |\\|     /            |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|             \\    |\\|    /             |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|              \\   |\\|   /              |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|               \\  (I)  /               |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                \\  U  /                |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                 \\   /                 |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                  \\ /                  |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                   V                   |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "|                                       |");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "-----------------------------------------");
-	wattroff(cor->vizu->win1, COLOR_PAIR((cor->winner->num) * (-1) + 8));
-}
-
-void win_art_winner(t_cor *cor)
-{
-	int x = 10;
-	int y = 70;
-
-	wattron(cor->vizu->win1, COLOR_PAIR((cor->winner->num) * (-1)));
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "              (           )        )             (     ");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "  (  (        )\\ )     ( /(     ( /(             )\\ )  ");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "  )\\))(   '  (()/(     )\\())    )\\())    (      (()/(  ");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", " ((_)()\\ )    /(_))   ((_)\\    ((_)\\     )\\      /(_)) ");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", " _(())\\_)()  (_))      _((_)    _((_)   ((_)    (_))   ");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", " \\ \\((_)/ /  |_ _|    | \\| |   | \\| |   | __|   | _ \\  ");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "  \\ \\/\\/ /    | |     | .` |   | .` |   | _|    |   /  ");
-	mvwprintw(cor->vizu->win1, x++, y, "%s", "   \\_/\\_/    |___|    |_|\\_|   |_|\\_|   |___|   |_|_\\  ");
-	wattroff(cor->vizu->win1, COLOR_PAIR((cor->winner->num) * (-1)));
-}
-          
-
-
-void gg_wp(t_cor *cor)
-{
-	
-	// mvwprintw(cor->vizu->win1, 40, 20, "%.*s", cor->winner->prog_name);
-	// wrefresh(cor->vizu->win1);
-	refresh_vizu(cor);
-	werase(cor->vizu->win1);
-	// wrefresh(cor->vizu->win2);
-	win_art_sword(cor, 10);
-	win_art_sword(cor, 144);
-	win_art_winner(cor);
-	mvwprintw(cor->vizu->win1, 20, 80, "--> %s <--",cor->winner->prog_name);
-	mvwprintw(cor->vizu->win1, 22, 80, "%s","Press Space to exit");
-	wrefresh(cor->vizu->win1);
-	// wrefresh(cor->vizu->win2);
-	while (1)
-	{
-		if (getchar() == 32)
-			return;
-	}
-
-}
-
-void					init_map(t_cor *cor)
+void	init_map(t_cor *cor)
 {
 	int i;
 
@@ -357,4 +66,3 @@ void					init_map(t_cor *cor)
 		cor->vizu->last_brakedown[i] = 0;
 	}
 }
-
