12f2865f002978527573b5e0d76b79008376a539
Vitalii BONDARENKO <vibondar@e1r3p1.unit.ua>
2018-09-25 16:37:21 +0300

    norm

--- libft/libft/src/ft_reverse_bits.c
+++ libft/libft/src/ft_reverse_bits.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_reverse_bits.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vibondar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/25 16:37:03 by vibondar          #+#    #+#             */
+/*   Updated: 2018/09/25 16:37:04 by vibondar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_reverse_bits(void *a, size_t size)
@@ -7,8 +19,8 @@ void	ft_reverse_bits(void *a, size_t size)
 	char	*bits;
 
 	i = 0;
-	if(!a)
-		return;
+	if (!a)
+		return ;
 	bits = (char*)a;
 	while (i != size - i)
 	{
